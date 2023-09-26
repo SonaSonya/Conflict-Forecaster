84f0ba929ffe916586d4c798a13968219f4cb569
SelivanovaSofia <selivanova.sa@edu.spbstu.ru>
2023-09-26 21:34:46 +0300

    Rename UCDPEventCounter to UCDPEventService

--- src/main/java/com/conflict/forecaster/service/UCDPEventService.java
+++ src/main/java/com/conflict/forecaster/service/UCDPEventService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UCDPEventCounter {
+public class UCDPEventService {
 
     @Autowired
     private UCDPEventCountRepository ucdpEventCountRepository;
