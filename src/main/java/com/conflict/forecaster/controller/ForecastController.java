38536a2ec100b51876fe6647d27f4e80b04192ab
SelivanovaSofia <selivanova.sa@edu.spbstu.ru>
2023-10-01 23:43:45 +0300

    Add service method for getting status

--- src/main/java/com/conflict/forecaster/controller/ForecastController.java
+++ src/main/java/com/conflict/forecaster/controller/ForecastController.java
@@ -3,6 +3,8 @@ package com.conflict.forecaster.controller;
 import com.conflict.forecaster.service.UCDPApiClient;
 import com.conflict.forecaster.database.UCDPEventCountRepository;
 import com.conflict.forecaster.database.UCDPEventRepository;
+import com.conflict.forecaster.service.UCDPEventService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -19,6 +21,9 @@ public class ForecastController {
 
     @Autowired
     private UCDPApiClient api;
+    @Autowired
+    private UCDPEventService ucdpEventService;
+
     @Autowired
     private UCDPEventRepository ucdpEventRepository;
     @Autowired
@@ -49,17 +54,16 @@ public class ForecastController {
         return "";
     }
 
+    // Получение статуса по загруженным данным, какие даты и страны в наличии
     @GetMapping("/forecaster/data/status")
-    public String status(Model model) {
-        // Получение статуса по загруженным данным, какие даты и страны в наличии
-        //TODO: start date, end date in database, for example ["19.0.5”, "23.0.6”], country ids with names, for example [553: Malawi, 432: Mali, 435: Mauritania]
-        return "";
+    public ObjectNode status(Model model) {
+        return ucdpEventService.getStatus();
     }
 
     @GetMapping("/forecaster/predict")
     public String predict(@RequestParam(name="country_id", required=true) int countryId,
-                          @RequestParam(name="timespan", required=true) int timespan,
                           @RequestParam(name="violence_type", required=true) int violenceType,
+                          @RequestParam(name="timespan", required=true) int timespan,
                           Model model) {
 
         // TODO: Получение предсказания для страны, типа насилия и временного промежутка
@@ -77,54 +81,6 @@ public class ForecastController {
 //            UCDPEventCount ucdpEventCount = new UCDPEventCount(((Long)c[0]).intValue(),(int)c[1] , (int)c[2], (int)c[3], ((BigDecimal)c[4]).intValue(), ((Long)c[5]).intValue());
 //            ucdpEventCountRepository.save(ucdpEventCount);
 //        }
-//
-//        return "parse-event";
-//    }
-
-//    @GetMapping("/predict")
-//    public String predict(Model model) throws IOException, ParseException {
-//
-//        // Prepare input timeseries data.
-//        double[] dataArray = new double[] {4, 6, 1, 1, 2, 3, 7, 3, 2, 1, 1, 1, 1, 0, 0, 2};
-//
-//        // Set ARIMA model parameters.
-//        int p = 3;
-//        int d = 0;
-//        int q = 3;
-//        int P = 1;
-//        int D = 1;
-//        int Q = 0;
-//        int m = 0;
-//        int forecastSize = 1;
-//
-//        ArimaParams params = new ArimaParams(p, d, q, P, D, Q, m);
-//
-//        // Obtain forecast result. The structure contains forecasted values and performance metric etc.
-//        ForecastResult forecastResult = Arima.forecast_arima(dataArray, forecastSize, params);
-//
-//        // Read forecast values
-//        double[] forecastData = forecastResult.getForecast(); // in this example, it will return { 2 }
-//        System.out.println(forecastData[0]);
-//
-//        // You can obtain upper- and lower-bounds of confidence intervals on forecast values.
-//        // By default, it computes at 95%-confidence level. This value can be adjusted in ForecastUtil.java
-//        double[] uppers = forecastResult.getForecastUpperConf();
-//        double[] lowers = forecastResult.getForecastLowerConf();
-//
-//        System.out.println(uppers[0]);
-//        System.out.println(lowers[0]);
-//
-//        // You can also obtain the root mean-square error as validation metric.
-//        double rmse = forecastResult.getRMSE();
-//        System.out.println(rmse);
-//
-//        // It also provides the maximum normalized variance of the forecast values and their confidence interval.
-//        double maxNormalizedVariance = forecastResult.getMaxNormalizedVariance();
-//        System.out.println(maxNormalizedVariance);
-//
-//        // Finally you can read log messages.
-//        String log = forecastResult.getLog();
-//        System.out.println(log);
 //
 //        return "parse-event";
 //    }
--- src/main/java/com/conflict/forecaster/service/UCDPEventService.java
+++ src/main/java/com/conflict/forecaster/service/UCDPEventService.java
@@ -3,8 +3,13 @@ package com.conflict.forecaster.service;
 import com.conflict.forecaster.database.entity.UCDPEvent;
 import com.conflict.forecaster.database.UCDPEventCountRepository;
 import com.conflict.forecaster.database.UCDPEventRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UCDPEventService {
@@ -14,6 +19,30 @@ public class UCDPEventService {
     @Autowired
     private UCDPEventRepository ucdpEventRepository;
 
+    public ObjectNode getStatus () {
+        UCDPEvent earliestEvent = ucdpEventRepository.findFirst1ByOrderByYearDescMonthDesc().get(0);
+        UCDPEvent latestEvent = ucdpEventRepository.findFirst1ByOrderByYearAscMonthAsc().get(0);
+        List<Object[]> countries = ucdpEventRepository.findCountries();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode countriesNode = mapper.createArrayNode();
+        ObjectNode status = mapper.createObjectNode();
+
+        for (Object[] country : countries)
+        {
+            ObjectNode countryNode = mapper.createObjectNode();
+            countryNode.put("id", country[0].toString());
+            countryNode.put("name", country[1].toString());
+
+            countriesNode.add(countryNode);
+        }
+
+        status.put("start_date", earliestEvent.getMonth() + ".0." + earliestEvent.getYear());
+        status.put("end_date", latestEvent.getMonth() + ".0." + latestEvent.getYear());
+        status.set("countries", countriesNode);
+
+        return status;
+    }
 
 
 }
