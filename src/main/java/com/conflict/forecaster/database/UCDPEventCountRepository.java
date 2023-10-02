e16ded6c72aca2a0b6393bc52be5c7d30bf6e7b1
SelivanovaSofia <selivanova.sa@edu.spbstu.ru>
2023-10-03 00:27:04 +0300

    Create request for prediction

--- src/main/java/com/conflict/forecaster/database/UCDPEventCountRepository.java
+++ src/main/java/com/conflict/forecaster/database/UCDPEventCountRepository.java
@@ -1,10 +1,16 @@
 package com.conflict.forecaster.database;
 
 import com.conflict.forecaster.database.entity.UCDPEventCount;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UCDPEventCountRepository extends CrudRepository<UCDPEventCount, Long> {
 
+    @Query("SELECT u FROM UCDPEventCount u WHERE u.countryId = :countryId AND u.typeOfViolence = :typeOfViolence ORDER BY u.year, u.month")
+    List<UCDPEventCount> findByCountryIdAndTypeOfViolence(@Param("countryId") int countryId, @Param("typeOfViolence") int typeOfViolence);
 }
