5d4eca8f0976f17a5ba0108807a176e67dc39e0d
SelivanovaSofia <selivanova.sa@edu.spbstu.ru>
2024-02-27 20:09:57 +0300

    add lstm new model and some fixes

--- src/main/java/com/conflict/forecaster/service/LSTM/LSTMPredictionService.java
+++ src/main/java/com/conflict/forecaster/service/LSTM/LSTMPredictionService.java
@@ -4,35 +4,60 @@ import com.conflict.forecaster.database.UCDPEventCountRepository;
 import com.conflict.forecaster.database.entity.UCDPEventCount;
 import com.conflict.forecaster.service.PredictionService;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
+import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
+import org.datavec.api.records.reader.impl.collection.ListStringRecordReader;
+import org.datavec.api.split.ListStringSplit;
+import org.datavec.api.writable.Writable;
 import org.deeplearning4j.datasets.datavec.SequenceRecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.iterator.INDArrayDataSetIterator;
 import org.deeplearning4j.eval.RegressionEvaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.*;
-import org.deeplearning4j.nn.conf.layers.LSTM;
-import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.conf.preprocessor.CnnToRnnPreProcessor;
+import org.deeplearning4j.nn.conf.preprocessor.RnnToCnnPreProcessor;
+import org.deeplearning4j.nn.conf.preprocessor.RnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.common.primitives.Pair;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.ViewIterator;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.NDArrayIndex;
+import org.nd4j.linalg.learning.RmsPropUpdater;
+import org.nd4j.linalg.learning.config.AdaGrad;
 import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.learning.config.IUpdater;
+import org.nd4j.linalg.learning.config.RmsProp;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 @Service
 public class LSTMPredictionService{
     private UCDPEventCountRepository ucdpEventCountRepository;
     private MultiLayerNetwork lstmModel;
     private PredictionService predictionService;
+
+    //Random number generator seed, for reproducibility
+    public static final int seed = 12345;
+    //Number of iterations per minibatch
+    public static final int iterations = 35;
+    //Number of epochs (full passes of the data)
+    public static final int nEpochs = 80;
+    //Number of data points
+    public static int nSamples = 25;
+    //Network learning rate
+    public static final double learningRate = 0.0001;
+
 
     @Autowired
     public LSTMPredictionService(UCDPEventCountRepository ucdpEventCountRepository, LSTMConfig lstmConfig, PredictionService predictionService) {
@@ -42,8 +67,117 @@ public class LSTMPredictionService{
         this.lstmModel.init();
     }
 
-    public void test2()
-    {
+    public void test() {
+        //Generate the training data
+        DataSet trainingData = getTrainingData();
+        trainingData.shuffle();
+        System.out.println(trainingData);
+        System.out.println();
+        DataSet testData = getTestData();
+        System.out.println(testData);
+
+        //Create the network
+        int numInput = 1;
+        int numOutputs = 1;
+        int nHidden = 30;
+
+//        NeuralNetConfiguration.Builder builder = new NeuralNetConfiguration.Builder();
+//        builder.seed(seed);
+//        builder.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT);
+//        builder.updater(Updater.RMSPROP);
+//        builder.gradientNormalization(GradientNormalization.ClipL2PerLayer);
+//        builder.gradientNormalizationThreshold(0.00001);
+//
+//        NeuralNetConfiguration.ListBuilder listBuilder = builder.list();
+//
+//        listBuilder.layer(0, new GravesLSTM.Builder().nIn(numInput).nOut(nHidden)
+//                .activation(Activation.TANH).l2(0.0001).weightInit(WeightInit.XAVIER)
+//                .build());
+//        listBuilder.layer(1, new GravesLSTM.Builder().nIn(nHidden).nOut(nHidden)
+//                .activation(Activation.TANH).l2(0.0001).weightInit(WeightInit.XAVIER)
+//                .build());
+//        listBuilder.layer(2, new RnnOutputLayer.Builder(LossFunctions.LossFunction.MSE)
+//                .activation(Activation.IDENTITY).l2(0.0001).weightInit(WeightInit.XAVIER)
+//                .nIn(nHidden).nOut(numOutputs).build());
+//        listBuilder.backpropType(BackpropType.Standard);
+//
+//        MultiLayerConfiguration conf = listBuilder.build();
+//        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+//        net.init();
+//        //net.setListeners(new HistogramIterationListener(1));
+//
+//        INDArray output;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .seed(seed)
+                .updater(new Adam())
+                .weightInit(WeightInit.XAVIER)
+                .list()
+                .layer(0, new LSTM.Builder()
+                        .nIn(numInput)
+                        .nOut(nHidden)
+                        .activation(Activation.TANH)
+                        .build())
+                .layer(1, new LSTM.Builder()
+                        .nIn(nHidden)
+                        .nOut(nHidden)
+                        .activation(Activation.TANH)
+                        .build())
+                .layer(2, new RnnOutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                        .activation(Activation.IDENTITY)
+                        .nIn(nHidden)
+                        .nOut(numOutputs)
+                        .gradientNormalization(GradientNormalization.ClipL2PerLayer)
+                        .gradientNormalizationThreshold(0.00001)
+                        .build())
+                .build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+
+        INDArray output;
+
+        //Train the network on the full data set
+        for( int i = 0; i < nEpochs; i++ ) {
+            // train the model
+            net.fit(trainingData);
+            output = net.rnnTimeStep(trainingData.getFeatures());
+            //System.out.println(output);
+            net.rnnClearPreviousState();
+        }
+
+
+//        System.out.println("Result on training data: ");
+//        System.out.println(net.rnnTimeStep(trainingData.getFeatures()));
+//        System.out.println(trainingData.getFeatures());
+//
+//        System.out.println();
+//
+//        System.out.println("Result on test data: ");
+//        System.out.println(net.rnnTimeStep(testData.getFeatures()));
+//        System.out.println(testData.getFeatures());
+
+
+        INDArray test = Nd4j.zeros(1, 1, 1);
+        test.putScalar(0, 1.00);
+        for (int i = 0; i < nSamples; i++) {
+            output = net.rnnTimeStep(test);
+            test.putScalar(0, output.getDouble(0));
+            System.out.print(" " + output);
+        }
+
+    }
+
+    /*
+        Generate the training data. The sequence to train is out = 1, 2, 3, ..., 100.
+        This corresponds to having as input the sequence seq = 0, 1, 2, ..., 99, so for this
+        training data set the input attribute sequence is seq and the class/target attribute is out.
+        The RNN should then be able to predict 101, 102, ... given the input 100, 101, ...
+        That is: the last output is the next input.
+     */
+    private DataSet getTrainingData() {
+
         int countryId = 520;
         int violenceType = 3;
         List<UCDPEventCount> ucdpEventCounts = ucdpEventCountRepository.findByCountryIdAndTypeOfViolence(countryId, violenceType);
@@ -55,66 +189,50 @@ public class LSTMPredictionService{
         }
         System.out.println();
 
-        int trainingDataPercentage = 80;
-        int totalDataSize = dataArray.length;
-        int testStartIndex = (int) (totalDataSize * (trainingDataPercentage / 100.0));
+        // Размер исходного массива данных
+        int dataSize = dataArray.length;
+        nSamples = dataSize - 1;
 
-        double[] trainingDataArray = Arrays.copyOfRange(dataArray, 0, testStartIndex);
-        double[] testDataArray = Arrays.copyOfRange(dataArray, testStartIndex, totalDataSize);
+        // Размер массива данных без меток
+        int inputDataSize = dataSize - 1;
 
-        this.trainModel(trainingDataArray, 10);
-        // Предсказание
-        List<Double> predictions = predict(dataArray, 10);
+        // Создаем массив для хранения входных данных
+        double[] seq = Arrays.copyOfRange(dataArray, 0, inputDataSize);
 
-        System.out.println("Predictions: " + predictions);
+        // Создаем массив для хранения меток
+        double[] out = Arrays.copyOfRange(dataArray, 1, dataSize);
 
-        return;
+        System.out.println("Массив входных данных:");
+        System.out.println(Arrays.toString(seq));
+
+        System.out.println("Массив меток:");
+        System.out.println(Arrays.toString(out));
+
+        // Scaling to [0, 1] based on the training output
+//        int min = 1;
+//        int max = nSamples;
+//        for(int i = 0; i < nSamples; i++) {
+//            seq[i] = (seq[i] - min)/(max - min);
+//            out[i] = (out[i] - min)/(max - min);
+//        }
+
+        INDArray seqNDArray = Nd4j.create(seq, new int[]{nSamples,1});
+        INDArray inputNDArray = Nd4j.zeros(1,1,nSamples);
+        //inputNDArray.putRow(0, seqNDArray.transpose());
+        inputNDArray.put(new INDArrayIndex[]{NDArrayIndex.point(0), NDArrayIndex.all()}, seqNDArray.transpose());
+
+        INDArray outNDArray = Nd4j.create(out, new int[]{nSamples,1});
+        INDArray outputNDArray = Nd4j.zeros(1,1,nSamples);
+        //outputNDArray.putRow(0, outNDArray.transpose());
+        outputNDArray.put(new INDArrayIndex[]{NDArrayIndex.point(0), NDArrayIndex.all()}, outNDArray.transpose());
+
+        DataSet dataSet = new DataSet(inputNDArray, outputNDArray);
+        return dataSet;
     }
 
-    public void test() {
-        int lstmLayerSize = 50;
-        int numEpochs = 100;
-        int nInput = 1;
-        int nOutput = 1;
+    private DataSet getTestData() {
 
-        INDArray dataX = generateRandomData(100, nInput);
-        INDArray dataY = generateRandomData(5, nOutput);
-
-        MultiLayerNetwork net = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
-                .seed(123)
-                .weightInit(WeightInit.XAVIER)
-                .updater(Updater.NONE)
-                .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue) // проверить
-                .gradientNormalizationThreshold(10.0)
-                .list()
-                .layer(0, new LSTM.Builder().nIn(nInput).nOut(lstmLayerSize)
-                        .activation(Activation.TANH).build())
-                .layer(1, new LSTM.Builder().nIn(lstmLayerSize).nOut(lstmLayerSize)
-                        .activation(Activation.TANH).build())
-                .layer(2, new org.deeplearning4j.nn.conf.layers.RnnOutputLayer.Builder(LossFunctions.LossFunction.MSE)
-                        .activation(Activation.IDENTITY)
-                        .nIn(lstmLayerSize).nOut(nOutput).build())
-                .backpropType(BackpropType.TruncatedBPTT)
-                .tBPTTForwardLength(10)
-                .tBPTTBackwardLength(10)
-                .pretrain(false)
-                .backprop(true)
-                .build()
-        );
-
-        net.init();
-        net.setListeners(new ScoreIterationListener(1));
-
-        for (int epoch = 0; epoch < numEpochs; epoch++) {
-            net.fit(dataX, dataY);
-            net.rnnClearPreviousState();
-        }
-
-        // предсказание
-        INDArray inputData = generateRandomData(5, nInput); // 5 временных точек для предсказания
-        INDArray predicted = net.rnnTimeStep(inputData);
-        System.out.println("inputData values: " + inputData);
-        System.out.println("Predicted values: " + predicted);
+        return getTrainingData();
     }
 
     private static INDArray generateRandomData(int numPoints, int numFeatures) {
@@ -141,23 +259,60 @@ public class LSTMPredictionService{
         return forecasts;
     }
 
-    public List<Double> predict(double[] inputData, int numTimeSteps) {
-        INDArray inputArray = Nd4j.create(inputData).reshape(1, 1, inputData.length);
+//    public List<Double> predict(double[] inputData, int numTimeSteps) {
+//        INDArray inputArray = Nd4j.create(inputData).reshape(1, 1, inputData.length);
+//
+//        // Прогноз
+//        INDArray forecastArray = lstmModel.rnnTimeStep(inputArray);
+//
+//        return getResponse(forecastArray);
+//    }
 
-        // Прогноз
-        INDArray forecastArray = lstmModel.rnnTimeStep(inputArray);
-
-        return getResponse(forecastArray);
-    }
-
-    public void trainModel(double[] trainingData, int numEpochs) {
-        INDArray inputArray = Nd4j.create(trainingData).reshape(1, 1, trainingData.length);
-        INDArray labelArray = Nd4j.create(trainingData).reshape(1, 1, trainingData.length);
-
+    public void trainModel(int numEpochs) {
+//
+//
+//
+//        int countryId = 520;
+//        int violenceType = 3;
+//        List<UCDPEventCount> ucdpEventCounts = ucdpEventCountRepository.findByCountryIdAndTypeOfViolence(countryId, violenceType);
+//
+//        double[] dataArray = predictionService.getArrayOfViolenceCounts(ucdpEventCounts);
+//        System.out.print("Data Array: ");
+//        for (double data : dataArray) {
+//            System.out.print(data + " ");
+//        }
+//        System.out.println();
+//
+//        int trainingDataPercentage = 80;
+//        int totalDataSize = dataArray.length;
+//        int testStartIndex = (int) (totalDataSize * (trainingDataPercentage / 100.0));
+//
+//        double[] trainingDataArray = Arrays.copyOfRange(dataArray, 0, testStartIndex);
+//        double[] testDataArray = Arrays.copyOfRange(dataArray, testStartIndex, totalDataSize);
+//
+//        val numSkipLines = 1;
+//        val regression = true;
+//        val batchSize = 32;
+//
+//        val trainFeatures = new CSVSequenceRecordReader(numSkipLines, ",");
+//        trainFeatures.initialize( new NumberedFileInputSplit(featureBaseDir + "/%d.csv", 1, 1600));
+//        val trainTargets = new CSVSequenceRecordReader(numSkipLines, ",");
+//        trainTargets.initialize(new NumberedFileInputSplit(targetsBaseDir + "/%d.csv", 1, 1600));
+//
+//        val train = new SequenceRecordReaderDataSetIterator(trainFeatures, trainTargets, batchSize,
+//                10, regression, SequenceRecordReaderDataSetIterator.AlignmentMode.EQUAL_LENGTH);
+//
+//        new SequenceRecordReaderDataSetIterator(SequenceRecordReader featuresReader, SequenceRecordReader labels,
+//        int miniBatchSize, int numPossibleLabels)
+//new ViewIterator();
+//        train = new SequenceRecordReaderDataSetIterator(trainFeatures, trainTargets, batchSize,
+//                10, true, SequenceRecordReaderDataSetIterator.AlignmentMode.EQUAL_LENGTH)
+//        //INDArray labelArray = Nd4j.create(trainingData).reshape(1, 1, trainingData.length);
+//        lstmModel.fit( inputArray, numEpochs );
         // Обучение
-        for (int epoch = 0; epoch < numEpochs; epoch++) {
-            lstmModel.fit(inputArray, labelArray);
-        }
+//        for (int epoch = 0; epoch < numEpochs; epoch++) {
+//            lstmModel.fit(inputArray, labelArray);
+//        }
     }
 
 
